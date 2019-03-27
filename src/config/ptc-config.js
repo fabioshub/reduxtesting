6c47e70da3d17aeba756e78b4a490ba6187d4b1f
Fabio F.J.P. De Bruijn <fabio@office.iw>
2019-03-27 11:12:16 +0100

    config updated

--- src/config/ptc-config.js
+++ src/config/ptc-config.js
@@ -1,6 +1,6 @@
-export const endpoint = 'http://localhost:58080';
-export const dataEndpoint = endpoint + '/data';
-export const itemEndpoint = endpoint + '/item';
+export const endpoint = 'https://staging.api.ptmd.nl/rest/b2c/v1/';
+export const dataEndpoint = endpoint + 'data';
+export const itemEndpoint = endpoint + 'item';
 export const itemImageEndpoint = 'https://api.ptmd.nl/rest/images/v1/bigger/'
 export const categoriesEndpoint = endpoint + '/categories'; //CURRENTLY NOT IN USE
 export const productGroupEndpoint = endpoint + '/allproductgroups'; //CURRENTLY NOT IN USE
